f886558555cb6d94f11eb7e1fab6d10e0e1815c4
Xerorb <150077220+Xerorb@users.noreply.github.com>
2024-04-16 15:16:37 -0400

    Added first enemy with basic AI (fix colors later)

--- Assets/Boss Arena/EnemyPathfinderAI.cs
+++ Assets/Boss Arena/EnemyPathfinderAI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[System.Serializable]
 
 public class EnemyPathfinderAI : MonoBehaviour
 { //initializing
@@ -21,6 +22,10 @@ public class EnemyPathfinderAI : MonoBehaviour
     {
         myRender = GetComponent<Renderer>();
         theRigidbody = GetComponent<Rigidbody>();
+        playerTargetDistance = 0f;
+        enemyLookDistance = 10f;
+        attackDistance = 8f;
+        enemyMovementSpeed = 0.6f;
         goRight = false;
         speed = 1.5f;
         interval = 2f;
@@ -28,10 +33,24 @@ public class EnemyPathfinderAI : MonoBehaviour
     }
 
     // Update is called once per x amount of time
-    //update at a fixed rate
     void FixedUpdate()
     {
         playerTargetDistance = Vector3.Distance(playerTarget.position, transform.position);
+        if (playerTargetDistance < attackDistance)
+        {
+            myRender.material.color = Color.red;
+            print("Attaaaacckkkk!");
+        }
+        else if (playerTargetDistance < enemyLookDistance)
+        {   
+            myRender.material.color = Color.yellow;
+            print("Be alert!");
+        }
+        else
+        {
+            myRender.material.color = Color.blue;
+            print("At ease, soldier.");
+        }
         if (goRight == true) // if the thing is going right
         {
             transform.Translate(speed * Time.deltaTime, 0, 0);
